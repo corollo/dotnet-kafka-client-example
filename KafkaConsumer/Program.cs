5872aa76d889cf97faf9abe9c093d7658fd5995a
corollo luigi giuseppe <lg.corollo@almaviva.it>
2019-04-16 12:05:15 +0200

    initial project version

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace KafkaConsumer
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+  using Confluent.Kafka;
+  using Confluent.Kafka.Serialization;
+
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+		Console.WriteLine("### KafkaConsumer - start");
+		
+      var config = new Dictionary<string, object>
+      {
+          { "group.id", "sample-consumer1" },
+          { "bootstrap.servers", "localhost:9092" },
+          { "enable.auto.commit", "false"}
+		  
+      };
+	  
+	  var topicName = "hello-topic";
+
+      using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+      {                
+        consumer.Subscribe(new string[]{topicName});
+
+        consumer.OnMessage += (_, msg) => 
+        {
+          Console.WriteLine($"############# Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+          consumer.CommitAsync(msg);
+        };
+		
+		//gestione errore 
+		consumer.OnError += (_, error) => 
+		{
+		  Console.WriteLine($"[ERROR] Error: {error}");
+		};
+
+        while (true)
+        {
+            consumer.Poll(100);
+        }
+      }
+    }
+  }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace KafkaProducer                                                                                                                    
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+  using Confluent.Kafka;
+  using Confluent.Kafka.Serialization;
+  
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+	  Console.WriteLine("### KafkaProducer - start");
+		
+      var config = new Dictionary<string, object>
+      {
+        { "bootstrap.servers", "localhost:9092" },
+        { "message.timeout.ms", 1000 },
+        { "message.send.max.retries", 0 }	    
+      };
+	  
+	  var topicName = "hello-topic";
+	  
+      using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+      {
+        Console.WriteLine($">> KafkaProducer {producer.Name} on {topicName}. quit to exit.");
+        string text = null;
+
+        while (text != "quit")
+        {
+          text = Console.ReadLine();                                                                                                                                                              
+          var deliveryReport = producer.ProduceAsync(topicName, null, text);
+		  
+		  //gestione errori invio
+		  deliveryReport.ContinueWith(task => { 		 
+			if (task.Result.Error.HasError) {
+				Console.WriteLine($"[ERROR-01] IS ERROR RESULT {deliveryReport.Result.Error.Reason}");
+			}
+			if (task.IsFaulted){
+				Console.WriteLine("[ERROR-02] IS FAULTED");
+			}
+			if (task.Exception != null){
+				Console.WriteLine("[ERROR-03] ex: {deliveryReport.Exception.Message}");
+			}
+			if (task.IsCanceled){
+				Console.WriteLine("[ERROR-04] IS CANCELLED");
+			}
+		  });
+		  
+        }
+
+        producer.Flush(100);
+		
+		Console.WriteLine("### KafkaProducer - end");
+      }
+    }
+  }
+}
